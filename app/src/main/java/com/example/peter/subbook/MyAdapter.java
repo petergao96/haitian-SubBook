9afce32188bf0b00624f271f5f8ad6dd40295fd6
FireVentus <petergao96@yahoo.ca>
2018-02-05 13:26:31 -0800

    APK

--- app/src/main/java/com/example/peter/subbook/MyAdapter.java
+++ app/src/main/java/com/example/peter/subbook/MyAdapter.java
@@ -20,24 +20,23 @@ public class MyAdapter extends ArrayAdapter<Subscription>{
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
-        // Get the data item for this position
+
         Subscription subs = getItem(position);
-        // Check if an existing view is being reused, otherwise inflate the view
+
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
-        // Find each text box
+
         TextView textName = (TextView) convertView.findViewById(R.id.TextName);
         TextView textCharge = (TextView) convertView.findViewById(R.id.TextCharge);
         TextView textComment = (TextView) convertView.findViewById(R.id.TextComment);
         TextView textDate = (TextView) convertView.findViewById(R.id.TextDate);
-        // Set the text of the elements
+
         textName.setText("Name: " +subs.getName());
         textCharge.setText("Charge: $"+subs.getCharge());
         textDate.setText("Date Started: "+subs.getFormDate());
         textComment.setText("Comments: "+subs.getComment());
 
-        // Return the completed view to render on screen
         return convertView;
     }
 
